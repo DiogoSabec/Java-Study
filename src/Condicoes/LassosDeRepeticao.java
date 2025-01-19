3fc7954d8732206c9cb046b88c5893b67676cbce
Diogo Sabec <diogovanzosabec@gmail.com>
2025-01-19 01:21:17 -0300

    Loops & Ternary

---
+++ src/Condicoes/LassosDeRepeticao.java
@@ -0,0 +1,22 @@
+package Condicoes;
+
+public class LassosDeRepeticao {
+    public static void main(String[] args) {
+
+        // while (condicao) {roda o codigo}
+        int numeroDeClones = 0;
+        int numeroMaxDeClones = 40;
+
+        while (numeroDeClones <= numeroMaxDeClones) {
+            System.out.println(numeroDeClones);
+            numeroDeClones++;
+        }
+
+        // For é um resumo de while.
+        for (int i = 0; i <= 40; i++) {
+            System.out.println(i);
+
+        }
+
+    }
+}
---
+++ src/Condicoes/Ternarios.java
@@ -0,0 +1,16 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+
+        // São maneiras usadas para reduzir o código
+        // variavel = (condicao) ? valorSeVerdadeiro : valorSeFalso;
+
+        short numeroDeMissoes = 1;
+        String nivelDoNinja = (numeroDeMissoes >= 10) ? "Esse ninja tem mais de 10 Missões" : "Esse ninja tem menos de 10 missoes";
+        System.out.println(nivelDoNinja);
+
+        String resultado = (2 == 2) ? "s" : "Não";
+        System.out.println("resultado = " + resultado);
+    }
+}
